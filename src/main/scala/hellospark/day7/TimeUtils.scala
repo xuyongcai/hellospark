cc3c5befaeb2e5f2a83b992f1678a854abf3da84
xuyongcai <15089565436@163.com>
2018-11-13 13:52:19 +0800

    add day7

---
+++ src/main/scala/hellospark/day7/TimeUtils.scala
@@ -0,0 +1,26 @@
+package hellospark.day7
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+
+object TimeUtils {
+
+  val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  val calendar = Calendar.getInstance()
+
+  def apply(time: String) = {
+    calendar.setTime(simpleDateFormat.parse(time))
+    calendar.getTimeInMillis
+  }
+
+  def getCertainDayTime(amount: Int): Long ={
+    calendar.add(Calendar.DATE, amount)
+    val time = calendar.getTimeInMillis
+    calendar.add(Calendar.DATE, -amount)
+    time
+  }
+
+
+
+}
